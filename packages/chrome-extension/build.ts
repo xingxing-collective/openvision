8c5a93aef343e490be3a730ca6b1547838f5e253
xingxingmofash <xingxingmofashu@outlook.com>
2026-02-26 17:55:17 +0800

    chore: add chrome extension build

---
+++ packages/chrome-extension/build.ts
@@ -0,0 +1,42 @@
+import { copy, remove } from 'fs-extra'
+
+const outdir = "./dist"
+
+await remove(outdir)
+await Bun.build({
+  entrypoints: ["./src/main.ts"],
+  outdir,
+})
+
+// Copy static assets
+await copy("./public", outdir)
+
+// Generate manifest.json
+type IconSize = "16" | "24" | "32" | "48" | "128"
+interface ManifestConfig {
+  manifest_version: 3
+  name: string
+  version: string
+  description: string
+  icons: Partial<Record<IconSize, string>>
+  action?: {
+    default_icon?: Partial<Record<IconSize, string>>
+    default_title?: string
+    default_popup?: string
+  }
+}
+
+async function generateManifestConfig(config: ManifestConfig) {
+  await Bun.write(`${outdir}/manifest.json`, JSON.stringify(config, null, 2))
+}
+
+await generateManifestConfig({
+  "name": "OpenVision",
+  "description": "Base Level Extension",
+  "version": "0.1",
+  "manifest_version": 3,
+  "icons": {
+    "128": "/images/openvision.png",
+  }
+})
+
