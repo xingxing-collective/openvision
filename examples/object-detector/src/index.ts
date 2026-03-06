4037f97cfde47eb3802bc160166704463c28d3f6
xingxingmofash <xingxingmofashu@outlook.com>
2026-03-06 17:35:24 +0800

    feat(object-detector): add object detection functionality with image display and styling
    - Implemented object detection in images using the ObjectDetector from openvision.
    - Added event listener to handle image clicks for detection.
    - Created a display function to show detection results with bounding boxes and confidence scores.
    - Introduced CSS styles for body and detection highlights using Tailwind CSS.
    - Added TypeScript configuration for the project.
    - Included a new image asset for testing purposes.

---
+++ examples/object-detector/src/index.ts
@@ -0,0 +1,95 @@
+import { ObjectDetector } from '../../../packages/openvision/src/index'
+
+/**
+ * Detect objects in still images on click
+ */
+async function handleClick(event: any) {
+  return await ObjectDetector.detect({
+    vision: {
+      basePath: "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm"
+    },
+    detector: {
+      options: {
+        baseOptions: {
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`,
+          delegate: "GPU"
+        },
+        scoreThreshold: 0.5,
+        runningMode: 'IMAGE'
+      }
+    },
+    detect: {
+      image: event.target
+    }
+  })
+}
+
+
+function displayImageDetections(
+  result: ObjectDetector.ObjectDetectorResult,
+  resultElement: HTMLImageElement
+) {
+  const ratio = resultElement.height / resultElement.naturalHeight;
+
+  for (let detection of result.detections) {
+    // Description text
+    const p = document.createElement("p");
+    p.setAttribute("class", "absolute p-0.5 bg-[#007f8b] text-white border-[1px,dashed,rgba(255, 255, 255, 0.7)]");
+    const highlighter = document.createElement("div");
+    highlighter.setAttribute("class", "bg-[#00ff0040] border-[1px,dashed,#fff] z-10 absolute");
+
+
+    if (detection.categories.length > 0) {
+      const category = detection.categories.at(0)!
+      p.innerText =
+        category.categoryName +
+        " - with " +
+        Math.round(parseFloat(`${category.score}`) * 100) +
+        "% confidence.";
+    }
+
+    // Positioned at the top left of the bounding box.
+    // Height is whatever the text takes up.
+    // Width subtracts text padding in CSS so fits perfectly.
+    if (detection.boundingBox) {
+      p.style =
+        "left: " +
+        detection.boundingBox?.originX * ratio +
+        "px;" +
+        "top: " +
+        detection.boundingBox.originY * ratio +
+        "px; " +
+        "width: " +
+        (detection.boundingBox.width * ratio) +
+        "px;";
+
+      highlighter.style =
+        "left: " +
+        detection.boundingBox.originX * ratio +
+        "px;" +
+        "top: " +
+        detection.boundingBox.originY * ratio +
+        "px;" +
+        "width: " +
+        detection.boundingBox.width * ratio +
+        "px;" +
+        "height: " +
+        detection.boundingBox.height * ratio +
+        "px;";
+
+    }
+
+    resultElement.parentNode?.appendChild(highlighter);
+    resultElement.parentNode?.appendChild(p);
+  }
+}
+
+window.onload = function () {
+  document.querySelectorAll(".detect").forEach((container) => {
+    container.querySelector('img')?.addEventListener('click', async function (e) {
+      const result = await handleClick(e)
+      displayImageDetections(result, this)
+    })
+  })
+
+}
